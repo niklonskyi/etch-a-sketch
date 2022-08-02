31e4f26c700ae1582ca72a81d7ddda629e84ad42
niklonskyi <niklonskyi@icloud.com>
2022-08-02 17:07:18 +0300

    Added button to change size of width and height of container.

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,26 @@
-let squareContainer = document.querySelector('.squares_container')
+let squareContainer = document.querySelector('div.squares-container')
+let sizeButton = document.querySelector('#size-of-container')
+let size = 10;
+let dimension = `${squareContainer.clientWidth/size}px`;
+
+sizeButton.addEventListener('click', function (){
+    size = prompt('Input a number of squares:');
+    squareContainer.innerHTML = '';
+    dimension = `${squareContainer.clientWidth/size}px`;
+    createGrid(size, dimension)
+})
 
 
-for (let i = 0; i < 16*16; i++) {
-    let square = document.createElement('div');
-    square.style.backgroundColor = Math.floor(Math.random()*16777215).toString(16);
-    square.addEventListener('mouseover', function (e) {
-        e.currentTarget.style.backgroundColor = `#${Math.floor(Math.random()*16777215).toString(16)}`;
-    }, false)
-    square.classList.add('square');
-    squareContainer.appendChild(square);
-
+function createGrid(size, demension) {
+    for (let i = 0; i < size * size; i++) {
+        let square = document.createElement('div');
+        square.addEventListener('mouseover', function (e) {
+            e.currentTarget.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+        }, false)
+        square.style.width = demension;
+        square.style.height = demension;
+        squareContainer.appendChild(square);
+    }
 }
+
+createGrid(size, dimension)
