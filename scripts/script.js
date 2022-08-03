b202c2c162afce80d217ba2eeaa2a97491c2989b
niklonskyi <niklonskyi@icloud.com>
2022-08-03 20:42:46 +0300

    Changed button to slider to change size of a container. Added few buttons, to change how squares react when hovered ('classic', 'colorful', 'lovely')

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,41 @@
 let squareContainer = document.querySelector('div.squares-container')
-let sizeButton = document.querySelector('#size-of-container')
+let sizeSlider = document.querySelector('#size-of-container')
 let size = 10;
 let dimension = `${squareContainer.clientWidth/size}px`;
+let sizeH3 = document.querySelector('#size-h3');
+let colors = document.querySelectorAll('.color');
+let colorValue = `classic`;
 
-sizeButton.addEventListener('click', function (){
-    size = prompt('Input a number of squares:');
-    squareContainer.innerHTML = '';
+sizeSlider.addEventListener('input', function (){
+    size = this.value;
     dimension = `${squareContainer.clientWidth/size}px`;
-    createGrid(size, dimension)
+    squareContainer.innerHTML = '';
+    createGrid(size, dimension);
 })
 
+for (let color of colors) {
+    color.addEventListener('click', function (){
+        colorValue = this.value;
+    })
+}
 
-function createGrid(size, demension) {
+function createGrid(size, dimension) {
+    sizeH3.textContent = `${size} x ${size}`;
     for (let i = 0; i < size * size; i++) {
         let square = document.createElement('div');
+        let opacity = 0.1;
         square.addEventListener('mouseover', function (e) {
-            e.currentTarget.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+            if (colorValue === 'classic'){
+                opacity += 0.1;
+                e.currentTarget.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
+            }
+            else if (colorValue === 'colorful'){
+                e.currentTarget.style.opacity = '0.8';
+                e.currentTarget.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+            }
         }, false)
-        square.style.width = demension;
-        square.style.height = demension;
+        square.style.width = dimension;
+        square.style.height = dimension;
         squareContainer.appendChild(square);
     }
 }
