24321723154c4ed1a61ae00b2240da0a890e8f58
niklonskyi <niklonskyi@icloud.com>
2022-08-08 00:01:15 +0300

    Added listeners to change squares while holding down right button of mouse, while hovering.

--- scripts/script.js
+++ scripts/script.js
@@ -1,47 +1,72 @@
+const COLOR_VALUE = `classic`;
+const SIZE = 10;
+
 let squareContainer = document.querySelector('div.squares-container')
 let sizeSlider = document.querySelector('#size-of-container')
-let size = 10;
-let dimension = `${squareContainer.clientWidth/size}px`;
 let sizeH3 = document.querySelector('#size-h3');
-let colors = document.querySelectorAll('.color');
-let colorValue = `classic`;
+let colorButtons = document.querySelectorAll('.color');
+let resetButton = document.querySelector('.reset')
+
+let size = SIZE;
+let colorValue = COLOR_VALUE;
+let dimension = `${squareContainer.clientWidth/size}px`;
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+function clearContainer(){
+    squareContainer.innerHTML = '';
+}
+
+function changeColor(e){
+    if (e.type === 'mouseover' && !mouseDown) return
+    if (colorValue === 'classic') {
+        e.currentTarget.style.backgroundColor = `rgba(0, 0, 0, 1)`;
+    } else if (colorValue === 'colorful') {
+        e.currentTarget.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    } else {
+        e.currentTarget.style.backgroundColor = `#${Math.floor(Math.random() * (16724732 - 16711680) + 16711680).toString(16)}`;
+    }
+}
+
+function createGrid(numberOfSquares, sizeOfSquare) {
+    sizeH3.textContent = `${numberOfSquares} x ${numberOfSquares}`;
+    for (let i = 0; i < numberOfSquares * numberOfSquares; i++) {
+        let square = document.createElement('div');
+        square.classList.add('square');
+        square.style.width = sizeOfSquare;
+        square.style.height = sizeOfSquare;
+        squareContainer.appendChild(square);
+    }
+    let squares = document.querySelectorAll('.square');
+    squareListeners(squares);
+}
+
+function squareListeners(squares){
+    for (const square of squares) {
+        square.addEventListener('mouseover', changeColor)
+        square.addEventListener('mousedown', changeColor)
+    }
+}
+
+
+resetButton.addEventListener('click', function () {
+    clearContainer();
+    createGrid(size, dimension);
+});
 
 sizeSlider.addEventListener('input', function (){
     size = this.value;
     dimension = `${squareContainer.clientWidth/size}px`;
-    squareContainer.innerHTML = '';
+    clearContainer();
     createGrid(size, dimension);
 })
 
-for (let color of colors) {
+for (let color of colorButtons) {
     color.addEventListener('click', function (){
         colorValue = this.value;
     })
-}
-
-function createGrid(size, dimension) {
-    sizeH3.textContent = `${size} x ${size}`;
-    for (let i = 0; i < size * size; i++) {
-        let square = document.createElement('div');
-        let opacity = 0.1;
-        square.addEventListener('mouseover', function (e) {
-            if (colorValue === 'classic'){
-                opacity += 0.1;
-                e.currentTarget.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
-            }
-            else if (colorValue === 'colorful'){
-                e.currentTarget.style.opacity = '0.8';
-                e.currentTarget.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-            }
-            else {
-                e.currentTarget.style.opacity = '0.5';
-                e.currentTarget.style.backgroundColor = `#${Math.floor(Math.random() * (16724732 - 16711680 ) + 16711680).toString(16)}`;
-            }
-        }, false)
-        square.style.width = dimension;
-        square.style.height = dimension;
-        squareContainer.appendChild(square);
-    }
 }
 
 createGrid(size, dimension)
